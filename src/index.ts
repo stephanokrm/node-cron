f18788ad177172f961fd995c4c37d749c9609446
Stephano Ramos <stephano.ramos@uber.com>
2023-05-15 22:25:33 -0300

    schedules trips

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import axios from 'axios';
+import Axios from 'axios';
 import cron from 'node-cron';
 
-const axiosInstance = Axios.create({
-  baseURL: 'https://service.schoolgo.com.br',
-  headers: {
-    Accept: "application/json",
-    "X-Requested-With": "XMLHttpRequest",
-  },
-  withCredentials: true,
+const axios = Axios.create({
+    baseURL: 'https://service.schoolgo.com.br',
+    headers: {
+        Accept: "application/json",
+        "X-Requested-With": "XMLHttpRequest",
+    },
+    withCredentials: true,
 });
 
 cron.schedule(`0 1 * * *`, async () => {
-  axiosInstance.post('https://service.schoolgo.com.br/api/trip/schedule');
+    const {status, statusText} = await axios.post('https://service.schoolgo.com.br/api/trip/schedule');
+
+    console.log({status, statusText})
 });
